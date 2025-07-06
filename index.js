173a5d12dfd9bd6a3ecdcb6eb987f24016f7f12e
MOHD ARIF <122776196+mrmohdarif@users.noreply.github.com>
2025-07-07 00:25:50 +0530

    aaa

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ res.send([
          
 
         "id": "1",
-        "name": "Mohd Arif alliswell",
+        "name": "Mohd",
         "author": "HT Entertainment Desk",
         "title": "Shah Rukh video calls 60-year-old fan battling cancer, promises financial help - Hindustan Times",
         "description": "Shah Rukh Khan recently video-called a 60-year-old fan battling cancer and promised to meet her soon as well as help her financially for the treatment. | Bollywood",
